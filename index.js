65735894d17b30c98911880f90dc8758fc4827b7
Sravan Chand <101858035+ogfourtwo@users.noreply.github.com>
2024-08-17 23:07:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ function playpause(){
 
 function changesource(n){  
     switch(n){
-        case 1:{x.src = "/album/01 Street Fighter.m4a";
+        case 1:{x.src = "./album/01 Street Fighter.m4a";
             document.getElementById("playing").innerHTML = "now playing: Street Fighter";
             break;
         }
-        case 2:{x.src = "/album/02 Strawberry Swing.m4a";
+        case 2:{x.src = "./album/02 Strawberry Swing.m4a";
             document.getElementById("playing").innerHTML = "now playing: Strawberry Swing";
             break;
         }
-        case 3:{x.src = "/album/03 Novacane.m4a";
+        case 3:{x.src = "./album/03 Novacane.m4a";
             document.getElementById("playing").innerHTML = "now playing: Novacane";
             break;
         }
-        case 4:{x.src = "/album/04 We All Try.m4a";
+        case 4:{x.src = "./album/04 We All Try.m4a";
             document.getElementById("playing").innerHTML = "now playing: We All Try";
             break;
         }
-        case 5:{x.src = "/album/10 Swim Good.m4a";
+        case 5:{x.src = "./album/10 Swim Good.m4a";
             document.getElementById("playing").innerHTML = "now playing: Swim Good";
             break;
         }
-        case 6:{x.src = "/album/13 Soul Calibur.m4a";
+        case 6:{x.src = "./album/13 Soul Calibur.m4a";
             document.getElementById("playing").innerHTML = "now playing: Soul Calibur";
             break;
         }
